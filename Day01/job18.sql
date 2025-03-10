6a904276ae943f5bd225f16a8fb4119da174ec84
rydeblack <ryhad.boughanmi@gmail.com>
2025-03-10 15:00:40 +0100

    job 23 done

--- Day01/job18.sql
+++ Day01/job18.sql
@@ -3,4 +3,4 @@ id	nom	prenom	age	email
 2	Steak	Chuck	45	chuck.steak@laplateforme.io
 4	Barnes	Binkie	16	binkie.barnes@laplateforme.io
 5	Dupuis	Gertrude	20	gertrude.dupuis@laplateforme.io
-6	Dupuis	Martin	18	martin.dupuis@laplateforme.io.
+6	Dupuis	Martin	18	martin.dupuis@laplateforme.io
---
+++ Day01/job23.sql
@@ -0,0 +1,2 @@
+id	nom	prenom	age	email
+2	Steak	Chuck	45	chuck.steak@laplateforme.io
